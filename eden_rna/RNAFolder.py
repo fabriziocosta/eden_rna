8902d4cb8926c72a0c6a1f0f2bbe86d8ec25017d
S Mautner <mautner@cs.uni-freiburg.de>
2017-04-03 23:34:31 +0200

    hayaku! all py files

--- eden_rna/RNAFolder.py
+++ eden_rna/RNAFolder.py
@@ -48,8 +48,9 @@ def extract_aligned_seed(header, out):
             if header in line:
                 seed += line.strip().split()[1]
     except:
-        import pdb
-        pdb.set_trace()
+        pass
+        #import pdb
+        #pdb.set_trace()
     return seed
 
 
@@ -169,19 +170,16 @@ class Vectorizer(object):
 
     def _align_sequence_structure(self, seq, neighs, structure_deletions=False):
         header = seq[0]
-        # if seq is in neigh rnaalifold will die...
-        seq_in_neighs=False
-        if seq in [y for x,y in neighs]:
-            seq_in_neighs=True
+        # if seq is in neigh rnaalifold will die...  rm duplicates :)
+        neighs = [ (a,b) for a,b in neighs if b!=seq[1]  ]
 
-        if len(neighs) < 1 or (seq_in_neighs and len(neighs)==2):
+        if len(neighs) < 1:
             clean_seq, clean_struct = rnafold.rnafold_wrapper(seq[1])
             energy = 0
             logger.debug('Warning: no alignment for: %s' % seq)
         else:
-            str_out = ""
-            if seq_in_neighs:
-                str_out = convert_seq_to_fasta_str(('',seq))
+
+            str_out = convert_seq_to_fasta_str(seq)
             for neigh in neighs:
                 str_out += convert_seq_to_fasta_str(neigh)
             cmd = 'echo "%s" | muscle -clwstrict -quiet' % (str_out)
